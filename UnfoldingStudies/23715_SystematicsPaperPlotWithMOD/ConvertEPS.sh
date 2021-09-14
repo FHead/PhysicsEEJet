53490252f1a56721e20a898c9b055024c0e4f5c3
FHead <chen.yi.first@gmail.com>
2021-09-14 16:14:02 +0200

    Add MOD and png options

--- UnfoldingStudies/23715_SystematicsPaperPlotWithMOD/ConvertEPS.sh
+++ UnfoldingStudies/23715_SystematicsPaperPlotWithMOD/ConvertEPS.sh
@@ -1,4 +1,5 @@
 for i in `ls Plots/ | grep eps$`
 do
    ps2pdf -dPDFSETTINGS=/prepress -dEPSCrop -dPDFFitPage Plots/$i Plots/${i/eps/pdf}
+   convert -density 150 Plots/${i/eps/pdf} Plots/${i/eps/png}
 done
