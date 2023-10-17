7d38f5ebd9f2cd17414a54bbcdd6b332a80a0d6a
FHead <chen.yi.first@gmail.com>
2023-10-17 16:25:19 -0500

    small fix to use basename

--- UnfoldingStudies/23755_UnfoldedPaperPlotWithMOD/ConvertEPS.sh
+++ UnfoldingStudies/23755_UnfoldedPaperPlotWithMOD/ConvertEPS.sh
@@ -1,5 +1,8 @@
-for i in `ls Plots/ | grep eps$`
+
+
+for i in `ls Plots/*${1}* | grep eps$`
 do
-   ps2pdf -dPDFSETTINGS=/prepress -dEPSCrop -dPDFFitPage Plots/$i Plots/${i/eps/pdf}
-   convert -density 150 Plots/${i/eps/pdf} Plots/${i/eps/png}
+   j=`basename $i`
+   ps2pdf -dPDFSETTINGS=/prepress -dEPSCrop -dPDFFitPage Plots/$j Plots/${j/eps/pdf}
+   convert -density 150 Plots/${j/eps/pdf} Plots/${j/eps/png}
 done
